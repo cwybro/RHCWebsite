a7889cdee9c751f7311c3d4c90c475f097c645c9
Connor <connor@wybro.com>
2017-11-05 12:41:02 -0500

    Add geocoder for location address

--- rhc/app/models/location.rb
+++ rhc/app/models/location.rb
@@ -1,3 +1,10 @@
 class Location < ApplicationRecord
   validates :title, :latitude, :longitude, presence: true
+
+  reverse_geocoded_by :latitude, :longitude
+  after_validation :reverse_geocode  # auto-fetch address
+
+  def address
+    Geocoder.address("#{latitude}, #{longitude}") || ""
+  end
 end
