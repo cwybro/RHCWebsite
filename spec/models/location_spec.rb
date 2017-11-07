ef88a5f7c89a4608d1de96679cd5918cb88432b4
Connor <connor@wybro.com>
2017-11-06 22:46:43 -0500

    Add test stubs to remove dependency on Geocode

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -19,21 +19,25 @@ RSpec.describe Location, type: :model do
 
   it "should have an address with valid coordinates" do
     park = Location.new(title: "Triangle Park", latitude: 42.825102, longitude: -75.548332)
+    allow(park).to receive(:reverse_geocode).and_return("10 Montgomery St, Hamilton, NY 13346, USA")
     expect(park.reverse_geocode).to eq "10 Montgomery St, Hamilton, NY 13346, USA"
   end
 
   it "should have a nil address with invalid coordinates" do
     invalid = Location.new(title: "Test place", latitude: -500.0, longitude: -500.0)
+    allow(invalid).to receive(:reverse_geocode).and_return(nil)
     expect(invalid.reverse_geocode).to eq nil
   end
 
   it "should have a short address with valid coordinates" do
     park = Location.new(title: "Triangle Park", latitude: 42.825102, longitude: -75.548332)
+    allow(park).to receive(:reverse_geocode).and_return("10 Montgomery St, Hamilton, NY 13346, USA")
     expect(park.short_address).to eq "Hamilton, NY - 13346"
   end
 
   it "should have an empty short address with invalid coordinates" do
     invalid = Location.new(title: "Test place", latitude: -500.0, longitude: -500.0)
+    allow(invalid).to receive(:reverse_geocode).and_return(nil)
     expect(invalid.short_address).to eq ""
   end
 end
