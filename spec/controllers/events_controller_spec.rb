5a6ed33b76b14fbe8bfe60192bd7d7a061aa5c3c
Franklin Van Nes <franklin@lastmileretail.com>
2017-11-06 23:21:22 -0500

    edit and create event controller tests

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -33,6 +33,19 @@ RSpec.describe EventsController, type: :controller do
   describe "POST #create" do
       it "returns http success and creates an event in the db" do
           event_params = {title: "15k christmas charity run",
+                datetime: "bad input",  # require 'date'
+                description: "Come and run the christmas 5K to raise money for the Madison Country Rural Health Council",
+                address: "Trudy Fitness Center, Hamilton"}
+          event = Event.new(event_params)
+          expect {
+              post :create, :params => {:event => event_params}
+          }.to change(Event, :count).by(0)
+          expect(response).to have_http_status(:redirect)
+          expect(response).to redirect_to(new_event_path)
+      end
+
+      it "return to the create new event page upon invalid information" do
+          event_params = {title: "15k christmas charity run",
                 datetime: DateTime.iso8601('2017-12-25T04:05:06-05:00'),  # require 'date'
                 description: "Come and run the christmas 5K to raise money for the Madison Country Rural Health Council",
                 address: "Trudy Fitness Center, Hamilton"}
@@ -41,6 +54,32 @@ RSpec.describe EventsController, type: :controller do
               post :create, :params => {:event => event_params}
           }.to change(Event, :count).by(1)
           expect(response).to have_http_status(:redirect)
+          expect(response).to redirect_to(events_path)
+      end
+  end
+
+  describe "PUT #update" do
+      it "returns http redirect and modifies an event in the db" do
+          event = Event.create(title: "5k christmas charity run",
+                datetime: DateTime.iso8601('2017-12-25T04:05:06-05:00'),  # require 'date'
+                description: "Come and run the christmas 5K to raise money for the Madison Country Rural Health Council",
+                address: "Trudy Fitness Center, Hamilton")
+          put :update, :params => {:id => event.id, :event => {:title => "10k run!"}}
+          event.reload
+          expect(event.title).to eq("10k run!")
+          expect(response).to have_http_status(:redirect)
+          expect(response).to redirect_to(event_url(event))
+      end
+
+      it "returns to the edit page after failure to edit event" do
+          event = Event.create(title: "5k christmas charity run",
+                datetime: DateTime.iso8601('2017-12-25T04:05:06-05:00'),  # require 'date'
+                description: "Come and run the christmas 5K to raise money for the Madison Country Rural Health Council",
+                address: "Trudy Fitness Center, Hamilton")
+          put :update, :params => {:id => event.id, :event => {:datetime => "sadlknasdlknasd"}}
+          event.reload
+          expect(event.datetime).to eq(DateTime.iso8601('2017-12-25T04:05:06-05:00'))
+          expect(response).to redirect_to(edit_event_url(event))
       end
   end
 
