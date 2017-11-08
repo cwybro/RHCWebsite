b7a6f8ebaa0d14d5ffce56e1f5c7425557f80868
Matthew Lee <mattdhlee@gmail.com>
2017-11-08 17:49:35 -0500

    geocode helper method added.

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -1,2 +1,15 @@
 module LocationsHelper
+    def geocode(address)
+      uri = URI.escape("https://maps.googleapis.com/maps/api/geocode/json?address=#{address}")
+      uri = URI.parse(uri)
+      uri.open do |f|
+        h = JSON.parse(f.read)
+        if h['status'] == 'OK' and h['results'].length > 0
+          lat = h['results'][0]['geometry']['location']['lat']
+          lon = h['results'][0]['geometry']['location']['lng']
+          return [lat.to_d, lon.to_d]
+        end
+      end
+      return [nil, nil]
+    end
 end
