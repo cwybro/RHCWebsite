b24fd0e7c8289f7225dd773f4d9c71731c2d6218
Matthew Lee <mattdhlee@gmail.com>
2017-11-08 17:49:35 -0500

    geocode helper method added.

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -1,9 +1,21 @@
 module LocationsHelper
-  def geocode_filter_location(location)
-    geo=Geokit::Geocoders::MultiGeocoder.geocode (location)
-    if !geo.success
-      return [nil, nil]
+    def geocode(address)
+        uri = URI.escape("https://maps.googleapis.com/maps/api/geocode/json?address=#{address}")
+        uri = URI.parse(uri)
+        uri.open do |f|
+            h = JSON.parse(f.read)
+            if (h['status'] == 'OK') && !h['results'].empty?
+                lat = h['results'][0]['geometry']['location']['lat']
+                lon = h['results'][0]['geometry']['location']['lng']
+                return [lat.to_d, lon.to_d]
+            end
+        end
+        [nil, nil]
     end
-    return [geo.lat, geo.lng] if geo.success
-  end
+
+    def geocode_filter_location(location)
+        geo = Geokit::Geocoders::MultiGeocoder.geocode location
+        return [nil, nil] unless geo.success
+        return [geo.lat, geo.lng] if geo.success
+    end
 end
