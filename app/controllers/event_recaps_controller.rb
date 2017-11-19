76a96a1877cd5fbe1cc491a958f1b5ed10e2e69e
qdvu124 <vudinhquan124@gmail.com>
2017-11-19 14:50:53 -0500

    Finished basic functionalities for creating and updating event recaps

--- app/controllers/event_recaps_controller.rb
+++ app/controllers/event_recaps_controller.rb
@@ -1,73 +1,63 @@
 class EventRecapsController < ApplicationController
   before_action :set_event_recap, only: [:show, :edit, :update, :destroy]
 
-  # GET /event_recaps
-  # GET /event_recaps.json
   # TODO: Don't actually need this
-  def index
-    @event_recaps = EventRecap.all
-  end
+  # def index
+  #   @event_recaps = EventRecap.all
+  # end
 
-  # GET /event_recaps/1
-  # GET /event_recaps/1.json
-  def show
-  end
+  # TODO: Don't actually need this
+  # def show
+  # end
 
-  # GET /event_recaps/new
   def new
     @event = Event.find(params[:event_id])
     @event_recap = EventRecap.new
   end
 
-  # GET /event_recaps/1/edit
   def edit
+    @event = Event.find(params[:event_id])
+    @event_recap = @event.event_recap
   end
 
-  # POST /event_recaps
-  # POST /event_recaps.json
   def create
     @event = Event.find(params[:event_id])
     @event_recap = EventRecap.new(event_recap_params)
-
-    respond_to do |format|
-      if @event_recap.save
-        format.html { redirect_to @event_recap, notice: 'Event recap was successfully created.' }
-        format.json { render :show, status: :created, location: @event_recap }
-      else
-        format.html { render :new }
-        format.json { render json: @event_recap.errors, status: :unprocessable_entity }
-      end
+    @event.event_recap = @event_recap
+    if @event_recap.save
+      flash[:notice] = 'Event recap was successfully created!'
+      redirect_to(event_path(@event))
+    else
+      flash[:alert] = 'Error encountered when creating event recap'
+      redirect_to(new_event_event_recap_path(@event))
     end
   end
 
-  # PATCH/PUT /event_recaps/1
-  # PATCH/PUT /event_recaps/1.json
   def update
-    respond_to do |format|
-      if @event_recap.update(event_recap_params)
-        format.html { redirect_to @event_recap, notice: 'Event recap was successfully updated.' }
-        format.json { render :show, status: :ok, location: @event_recap }
-      else
-        format.html { render :edit }
-        format.json { render json: @event_recap.errors, status: :unprocessable_entity }
-      end
+    @event = Event.find(params[:event_id])
+    if @event_recap.update(event_recap_params)
+      flash[:notice] = 'Event recap was successfully updated!'
+      @event.event_recap = @event_recap
+      redirect_to(event_path(@event))
+    else
+      flash[:alert] = 'Error encountered when creating event recap'
+      redirect_to(new_event_event_recap_path(@event))
     end
   end
 
-  # DELETE /event_recaps/1
-  # DELETE /event_recaps/1.json
-  def destroy
-    @event_recap.destroy
-    respond_to do |format|
-      format.html { redirect_to event_recaps_url, notice: 'Event recap was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  # TODO: Don't actually need this
+  # def destroy
+  #   @event_recap.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to event_recaps_url, notice: 'Event recap was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_event_recap
-      @event_recap = EventRecap.find(params[:id])
+      @event_recap = Event.find(params[:event_id]).event_recap
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- app/views/event_recaps/_event_recap.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! event_recap, :id, :attendance, :description, :created_at, :updated_at
-json.url event_recap_url(event_recap, format: :json)
