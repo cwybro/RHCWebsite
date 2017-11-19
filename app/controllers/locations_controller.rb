d2a1a22f1420aa15c21c421b88fff4c6f859b841
joloughlin <joloughlin@colgate.edu>
2017-11-19 16:22:39 -0500

    filter on distance form, location model methods to enable distance filtering

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,6 +1,8 @@
 class LocationsController < ApplicationController
   def index
     @locations = Location.all
+
+    
   end
 
   def show
--- app/models/location.rb
+++ app/models/location.rb
@@ -8,12 +8,52 @@ class Location < ApplicationRecord
   acts_as_mappable
   before_validation :geocode_address, :on => :create
 
-  private
+  def distance_from(location)
+    if location.nil? or location[0].nil? or location[1].nil?
+      return Float::INFINITY
+    end
+    if latitude.nil? or longitude.nil?
+      return Float::INFINITY
+    end
+
+    lat2 = to_rad location[0]
+    lon2 = to_rad location[1]
+
+    lat1 = to_rad latitude
+    lon1 = to_rad longitude
+
+    r = 3959
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = (Math.sin(dlat/2.0))**2.0+Math.cos(lat1) * Math.cos(lat2) * (Math.sin(dlon/2.0))**2
+    c = 2.0 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+    d = r * c
+end
+
+def distance_within?(location, distance)
+  if location.is_a? String
+    location = geocode(location)
+  end
+  d = distance_from(location)
+  if d.nil?
+    false
+  else
+     d <= distance
+  end
+end
+
+private
   def geocode_address
     geo=Geokit::Geocoders::MultiGeocoder.geocode (address)
     errors.add(:address, "Could not Geocode address") if !geo.success
     self.lat, self.lng = geo.lat,geo.lng if geo.success
   end
+
+  def to_rad(deg)
+    deg/180.0 * Math::PI
+  end
+
+
 
   # reverse_geocoded_by :latitude, :longitude
   # after_validation :reverse_geocode  # auto-fetch address
