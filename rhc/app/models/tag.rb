180e2afea04aa1a048ed7b8e9dbce14c3d1c5441
Matthew Lee <mattdhlee@gmail.com>
2017-11-19 14:52:37 -0500

    reverting to a49d9de1143807e3386518336a72e273abf1680f

--- rhc/app/models/tag.rb
+++
@@ -1,5 +0,0 @@
-class Tag < ApplicationRecord
-    has_many :taggings, dependent: :destroy
-    has_many :locations, through: :taggings
-    has_many :events, through: :taggings
-end
--- rhc/spec/models/tagging_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Tagging, type: :model do
-
-end
