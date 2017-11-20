d99abacf571e9b0324b22fba7b5eb5e163e9ba60
qdvu124 <vudinhquan124@gmail.com>
2017-11-19 23:50:48 -0500

    Finished adding recap model
    Finished adding recap model
    
    Finished adding recap model

--- app/models/recap.rb
+++ app/models/recap.rb
@@ -1,3 +1,5 @@
 class Recap < ApplicationRecord
     belongs_to :event
+    validates :attendance, :description, presence: true
+    validates :attendance, numericality: true
 end
--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe EventsController, type: :controller do
             description: "Come and run the christmas 5K to raise money for the Madison Country Rural Health Council",
             address: "Trudy Fitness Center, Hamilton")
       expect(Event).to receive(:find).with("1") {event}
-      get :edit, :params => { :id => 1}
+      get :edit, :params => {:id => 1}
       expect(response).to have_http_status(:success)
       expect(response).to render_template(:edit)
     end
@@ -31,56 +31,56 @@ RSpec.describe EventsController, type: :controller do
   end
 
   describe "POST #create" do
-      it "returns http success and creates an event in the db" do
-          event_params = {title: "15k christmas charity run",
-                datetime: "bad input",  # require 'date'
-                description: "Come and run the christmas 5K to raise money for the Madison Country Rural Health Council",
-                address: "Trudy Fitness Center, Hamilton"}
-          event = Event.new(event_params)
-          expect {
-              post :create, :params => {:event => event_params}
-          }.to change(Event, :count).by(0)
-          expect(response).to have_http_status(:redirect)
-          expect(response).to redirect_to(new_event_path)
-      end
+    it "returns http success and creates an event in the db" do
+        event_params = {title: "15k christmas charity run",
+              datetime: "bad input",  # require 'date'
+              description: "Come and run the christmas 5K to raise money for the Madison Country Rural Health Council",
+              address: "Trudy Fitness Center, Hamilton"}
+        event = Event.new(event_params)
+        expect {
+            post :create, :params => {:event => event_params}
+        }.to change(Event, :count).by(0)
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(new_event_path)
+    end
 
-      it "return to the create new event page upon invalid information" do
-          event_params = {title: "15k christmas charity run",
-                datetime: DateTime.iso8601('2017-12-25T04:05:06-05:00'),  # require 'date'
-                description: "Come and run the christmas 5K to raise money for the Madison Country Rural Health Council",
-                address: "Trudy Fitness Center, Hamilton"}
-          event = Event.new(event_params)
-          expect {
-              post :create, :params => {:event => event_params}
-          }.to change(Event, :count).by(1)
-          expect(response).to have_http_status(:redirect)
-          expect(response).to redirect_to(events_path)
-      end
+    it "return to the create new event page upon invalid information" do
+        event_params = {title: "15k christmas charity run",
+              datetime: DateTime.iso8601('2017-12-25T04:05:06-05:00'),  # require 'date'
+              description: "Come and run the christmas 5K to raise money for the Madison Country Rural Health Council",
+              address: "Trudy Fitness Center, Hamilton"}
+        event = Event.new(event_params)
+        expect {
+            post :create, :params => {:event => event_params}
+        }.to change(Event, :count).by(1)
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(events_path)
+    end
   end
 
   describe "PUT #update" do
-      it "returns http redirect and modifies an event in the db" do
-          event = Event.create(title: "5k christmas charity run",
-                datetime: DateTime.iso8601('2017-12-25T04:05:06-05:00'),  # require 'date'
-                description: "Come and run the christmas 5K to raise money for the Madison Country Rural Health Council",
-                address: "Trudy Fitness Center, Hamilton")
-          put :update, :params => {:id => event.id, :event => {:title => "10k run!"}}
-          event.reload
-          expect(event.title).to eq("10k run!")
-          expect(response).to have_http_status(:redirect)
-          expect(response).to redirect_to(event_url(event))
-      end
+    it "returns http redirect and modifies an event in the db" do
+        event = Event.create(title: "5k christmas charity run",
+              datetime: DateTime.iso8601('2017-12-25T04:05:06-05:00'),  # require 'date'
+              description: "Come and run the christmas 5K to raise money for the Madison Country Rural Health Council",
+              address: "Trudy Fitness Center, Hamilton")
+        put :update, :params => {:id => event.id, :event => {:title => "10k run!"}}
+        event.reload
+        expect(event.title).to eq("10k run!")
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(event_url(event))
+    end
 
-      it "returns to the edit page after failure to edit event" do
-          event = Event.create(title: "5k christmas charity run",
-                datetime: DateTime.iso8601('2017-12-25T04:05:06-05:00'),  # require 'date'
-                description: "Come and run the christmas 5K to raise money for the Madison Country Rural Health Council",
-                address: "Trudy Fitness Center, Hamilton")
-          put :update, :params => {:id => event.id, :event => {:datetime => "sadlknasdlknasd"}}
-          event.reload
-          expect(event.datetime).to eq(DateTime.iso8601('2017-12-25T04:05:06-05:00'))
-          expect(response).to redirect_to(edit_event_url(event))
-      end
+    it "returns to the edit page after failure to edit event" do
+        event = Event.create(title: "5k christmas charity run",
+              datetime: DateTime.iso8601('2017-12-25T04:05:06-05:00'),  # require 'date'
+              description: "Come and run the christmas 5K to raise money for the Madison Country Rural Health Council",
+              address: "Trudy Fitness Center, Hamilton")
+        put :update, :params => {:id => event.id, :event => {:datetime => "sadlknasdlknasd"}}
+        event.reload
+        expect(event.datetime).to eq(DateTime.iso8601('2017-12-25T04:05:06-05:00'))
+        expect(response).to redirect_to(edit_event_url(event))
+    end
   end
 
   describe "GET #show" do
@@ -90,13 +90,13 @@ RSpec.describe EventsController, type: :controller do
             description: "Come and run the christmas 5K to raise money for the Madison Country Rural Health Council",
             address: "Trudy Fitness Center, Hamilton")
       expect(Event).to receive(:find).with("1") {event}
-      get :show, :params => { :id => 1}
+      get :show, :params => {:id => 1}
       expect(response).to have_http_status(:success)
       expect(response).to render_template(:show)
     end
 
     it "redirects to index with bad id" do
-      get :show, :params => { :id => 2}
+      get :show, :params => {:id => 2}
       expect(response).to have_http_status(302)
       expect(response).to redirect_to(events_path)
     end
