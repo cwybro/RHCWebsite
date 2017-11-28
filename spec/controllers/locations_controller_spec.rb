f8a2f919aefff6b58af50eafb07b7d9044c17ffc
Connor <connor@wybro.com>
2017-11-27 22:09:12 -0500

    Update location controller tests

--- spec/controllers/locations_controller_spec.rb
+++ spec/controllers/locations_controller_spec.rb
@@ -29,14 +29,27 @@ RSpec.describe LocationsController, type: :controller do
   end
 
   describe "GET #new" do
-    it "returns http success" do
+    it "returns http success with admin user" do
+      login_with create(:user, :admin)
       get :new
       expect(response).to have_http_status(:success)
+    end
+
+    it "redirects to locations path with regular user" do
+      login_with create(:user)
+      get :new
+      expect(response).to redirect_to(locations_path)
+    end
+
+    it "redirects to locations with no user" do
+      get :new
+      expect(response).to redirect_to(locations_path)
     end
   end
 
   describe "POST #create" do
-      it "returns http success and creates a location in the db" do
+      it "returns http success and creates a location in the db for admin" do
+        login_with create(:user, :admin)
           location_params = {title: "Persson Steps",
                 description: "Many steps. Good workout route to classes",
                 address: "13 Oak Dr. Hamilton, NY"}
@@ -47,7 +60,28 @@ RSpec.describe LocationsController, type: :controller do
           expect(response).to have_http_status(:redirect)
           expect(response).to redirect_to(locations_path)
       end
+
+      it "does not create new location and redirects to locations if not admin" do
+        location_params = {title: "Persson Steps",
+              description: "Many steps. Good workout route to classes",
+              address: "13 Oak Dr. Hamilton, NY"}
+        location = Location.new(location_params)
+        expect {
+            post :create, :params => {:location => location_params}
+        }.to change(Location, :count).by(0)
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(locations_path)
+      end
+
+      it "does not create invalid location and redirects to locations if admin" do
+        login_with create(:user, :admin)
+        location_params = {title: "Persson Steps"}
+        expect {
+            post :create, :params => {:location => location_params}
+        }.to change(Location, :count).by(0)
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(new_location_path)
+        expect(flash[:warning]).to eq("Error creating new location")
+      end
   end
-
-
 end
