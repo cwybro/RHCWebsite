1739211e072cd6d722d69ac6140b745943d0dcba
Connor <connor@wybro.com>
2017-12-01 18:27:55 -0500

    Update admin styling & code cleanup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,5 @@
 class HomeController < ApplicationController
   def index
     @admin = current_user.try(:admin?)
-
-    # @location_img = Location.first.image.url(:grid) unless !Location.first.image.present?
-    # @event_img = Event.first.image.url(:thumb) unless !Event.first.image.present?
   end
 end
