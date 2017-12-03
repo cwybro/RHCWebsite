8fd8b22879b6920ac6d9b6afb247676e2e34208a
qdvu124 <vudinhquan124@gmail.com>
2017-12-03 17:29:11 -0500

    Refactored specs for RecapsController

--- spec/controllers/recaps_controller_spec.rb
+++ spec/controllers/recaps_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe RecapsController, type: :controller do 
+<<<<<<< ab103f21851de87c53ab2e93cd8c8ac553d3c1ec
   before(:each) do
     @user = create(:user)
     @event = create(:event)
@@ -28,72 +29,91 @@ RSpec.describe RecapsController, type: :controller do
     it "should return http success with valid event_id" do
       get :new, :params => {:event_id => @event.id}
       expect(response).to have_http_status(:success)
+=======
+  describe "#new and #create suite" do
+    before(:each) do
+      # See factories.rb
+      @user = create(:user)
+      @event = create(:event)
+>>>>>>> Refactored specs for RecapsController
     end
 
-    it "should redirect with invalid event_id" do
-      get :new, :params => {:event_id => -1}
-      expect(response).to have_http_status(:redirect)
-      expect(response).to redirect_to(events_path)
-    end
-  end
+    describe "GET #new" do
+      it "should return http success with valid event_id" do
+        get :new, :params => {:event_id => @event.id}
+        expect(response).to have_http_status(:success)
+      end
 
-  describe "POST #create" do
-    it "should return http success and create a recap associated with an event" do
-      expect {
-        post :create, :params => {:event_id => @event.id, :recap => recap_params(@event.id)}
-      }.to change(Recap, :count).by(1)
-      expect(response).to have_http_status(:redirect)
-      expect(response).to redirect_to(event_path(@event))
+      it "should redirect with invalid event_id" do
+        get :new, :params => {:event_id => -1}
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(events_path)
+      end
     end
 
-    it "should redirect with invalid event_id" do
-      expect {
-        post :create, :params => {:event_id => -1, :recap => recap_params(@event.id)}
-      }.to change(Recap, :count).by(0)
-      expect(response).to have_http_status(:redirect)
-      expect(response).to redirect_to(events_path)
-    end
+    describe "POST #create" do
+      it "should return http success and create a recap associated with an event" do
+        expect {
+          post :create, :params => {:event_id => @event.id, :recap => recap_params(@event.id)}
+        }.to change(Recap, :count).by(1)
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(event_path(@event))
+      end
 
-    it "should redirect with invalid recap" do
-      expect {
-        post :create, :params => {:event_id => 1, :recap => {:attendance => 'acbsdf'}}
-      }.to change(Recap, :count).by(0)
-      expect(response).to have_http_status(:redirect)
-      expect(response).to redirect_to(new_event_recap_path(@event))
-    end
-  end
+      it "should redirect with invalid event_id" do
+        expect {
+          post :create, :params => {:event_id => -1, :recap => recap_params(@event.id)}
+        }.to change(Recap, :count).by(0)
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(events_path)
+      end
 
-  describe "GET #edit" do
-    it "should return http success and show the edit page" do
-      recap = create(:recap)
-      expect(Event).to receive(:find).with("1") {@event}
-      get :edit, :params => {:event_id => @event.id, :id => @event.id}
-      expect(response).to have_http_status(:success)
-      expect(response).to render_template(:edit)
-    end
-
-    it "should return http failure on invalid event_id" do
-      recap = create(:recap)
-      get :edit, :params => {:event_id => -1, :id => -1}
-      expect(response).to have_http_status(:redirect)
-      expect(response).to redirect_to(events_path)
+      it "should redirect with invalid recap" do
+        expect {
+          post :create, :params => {:event_id => 1, :recap => {:attendance => 'acbsdf'}}
+        }.to change(Recap, :count).by(0)
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(new_event_recap_path(@event))
+      end
     end
   end
 
-  describe "POST #update" do
-    it "should return http success and show the event page" do
-      recap = create(:recap)
-      post :update, :params => {:event_id => @event.id, :id => @event.id, :recap => recap_params(@event.id)}
-      expect(response).to have_http_status(:redirect)
-      expect(response).to redirect_to(event_path(@event))
+  describe "edit and #update suite" do
+    before(:each) do
+      # See factories.rb
+      @user = create(:user)
+      @event = create(:event)
+      @recap = create(:recap)
     end
 
-    it "should return to the edit recap page upon failure to update" do
-      recap = create(:recap)
-      post :update, :params => {:event_id => 1, :id => 1, :recap => {:attendance => "asdfasc"}}
-      @event.reload
-      expect(@event.recap).to eq(recap)
-      expect(response).to redirect_to(edit_event_recap_path(@event))
+    describe "GET #edit" do
+      it "should return http success and show the edit page" do
+        expect(Event).to receive(:find).with("1") {@event}
+        get :edit, :params => {:event_id => @event.id, :id => @event.id}
+        expect(response).to have_http_status(:success)
+        expect(response).to render_template(:edit)
+      end
+
+      it "should return http failure on invalid event_id" do
+        get :edit, :params => {:event_id => -1, :id => -1}
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(events_path)
+      end
+    end
+
+    describe "POST #update" do
+      it "should return http success and show the event page" do
+        post :update, :params => {:event_id => @event.id, :id => @event.id, :recap => recap_params(@event.id)}
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(event_path(@event))
+      end
+
+      it "should return to the edit recap page upon failure to update" do
+        post :update, :params => {:event_id => 1, :id => 1, :recap => {:attendance => "asdfasc"}}
+        @event.reload
+        expect(@event.recap).to eq(@recap)
+        expect(response).to redirect_to(edit_event_recap_path(@event))
+      end
     end
   end
 
