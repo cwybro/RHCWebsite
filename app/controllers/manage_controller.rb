a668080efb31a906c6d8dffad15eceae1d80f08a
Connor <connor@wybro.com>
2017-12-08 11:44:36 -0500

    Fix missing date header for events on manage

--- app/controllers/manage_controller.rb
+++ app/controllers/manage_controller.rb
@@ -2,8 +2,10 @@ class ManageController < ApplicationController
   def index
     if current_user.try(:admin?)
       @user_events = Event.all
+      @now = DateTime.now
     else
       @user_events = Event.where('user_id == ?', (current_user.id))
+      @now = DateTime.now
     end
   end
 end
