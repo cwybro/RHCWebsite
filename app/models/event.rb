3caaa508f01e327f9196b200851e8056eb386f34
Matthew Lee <mattdhlee@gmail.com>
2017-12-08 11:50:55 -0500

    featured and future scope added

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,6 +7,8 @@ class Event < ApplicationRecord
     validates :is_reviewed, inclusion: { in: [ true, false ] }
     validate :timeliness_of_datetime
     scope :today, -> { where('datetime > ? AND datetime < ?', DateTime.current().beginning_of_day, DateTime.current().end_of_day) }
+    scope :future, -> { where('datetime > ?', DateTime.current().beginning_of_day) }
+    scope :featured_events, -> {where("featured == true")}
     has_many :taggings, dependent: :destroy
     has_many :tags, through: :taggings
     has_attached_file :image,
