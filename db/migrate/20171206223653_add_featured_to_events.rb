288f944fc39c3d97f88a42e8045072a9ee3b4fec
Matthew Lee <mattdhlee@gmail.com>
2017-12-12 19:42:52 -0500

    added migration with default value for featured column

--- db/migrate/20171206223653_add_featured_to_events.rb
+++
@@ -1,5 +0,0 @@
-class AddFeaturedToEvents < ActiveRecord::Migration[5.1]
-  def change
-    add_column :events, :featured, :boolean, :default => false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20171213004825) do
+=======
+ActiveRecord::Schema.define(version: 20171213003410) do
+>>>>>>> added migration with default value for featured column
 
   create_table "events", force: :cascade do |t|
     t.text "title"
